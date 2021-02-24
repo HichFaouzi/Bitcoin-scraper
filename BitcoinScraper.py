da7d09d2e692a0dbaed3b9f53f92f3057e2adf17
HichFaouzi <39406490+HichFaouzi@users.noreply.github.com>
2021-02-24 21:05:27 +0100

    Update BitcoinScraper.py

--- BitcoinScraper.py
+++ BitcoinScraper.py
@@ -24,9 +24,7 @@ def scraper():
     for i in Hashes:
         Hashes2.append(i.text)
     def chunks(l, n):
-        # For item i in a range that is a length of l,
         for i in range(0, len(l), n):
-            # Create an index range for l of n items:
             yield l[i:i+n]
     FinalList = list(chunks(Values2, 3))
 
